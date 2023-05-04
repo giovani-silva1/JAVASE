e383d537692ae2c39a7a8876ed8f52cad86a97ec
Giovani Silva <giovani-silva1@live.com>
2023-05-04 19:39:27 -0300

    Subida do projeto MasterClass JAVASE

---
+++ MasterClass-JavaSE/src/br/com/javase/basics/UseIfElse.java
@@ -0,0 +1,14 @@
+package br.com.javase.basics;
+
+public class UseIfElse {
+
+    public static void main(String[] args) {
+        boolean isAlien = false;
+
+        if(isAlien == false){
+            System.out.println("There aren't Alien here");
+        }else{
+            System.out.println("There are Alien here");
+        }
+    }
+}
